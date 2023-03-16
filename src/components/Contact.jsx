c4a4003a022ce35475d40633a1896968149039ed
janetuwili <janetuwili@gmail.com>
2023-03-16 13:08:29 -0700

    commit one

---
+++ src/components/Contact.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+ function Contact() {
+  return (
+    <div className='contact-form'>Contact</div>
+  )
+}
+export default Contact
---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from './Nav';
+import Footer from './Footer';
+ function Home() {
+  return (
+    <div className='content text-white '>
+        <h2 className='pt-5'>ACADEMY FOR WOMEN ENTREPRENEUR RWANDA</h2>
+        <p>Are you a Rwandan Woman Entrepreneurs willing to be Empowered & Equiped with necessary skills</p> 
+        <p>to boost your business? The Academy for Women Entrepreneurs, is here for you.</p>
+    </div>
+  )
+}
+
+export default Home;
